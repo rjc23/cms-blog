3a290284e2f7f553f67599c1f3cb1fb5b56eebc1
Ryan J Carmody <ryancarmody@outlook.com>
2022-11-03 19:32:14 +1030

    plugin.js

--- config/plugins.js
+++ config/plugins.js
@@ -14,18 +14,18 @@ module.exports = ({ env }) => ({
     },
   });
 
-module.exports = ({ env }) => ({
-  graphql: {
-    config: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
-    },
-  },
-  });
+// module.exports = ({ env }) => ({
+//   graphql: {
+//     config: {
+//       endpoint: '/graphql',
+//       shadowCRUD: true,
+//       playgroundAlways: false,
+//       depthLimit: 7,
+//       amountLimit: 100,
+//       apolloServer: {
+//         tracing: false,
+//       },
+//     },
+//   },
+//   });
    
